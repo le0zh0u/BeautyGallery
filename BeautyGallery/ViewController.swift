4554f38f70ab15aeabd9b0581cdd7c5fbc44c75c
leozhou <492753662@qq.com>
2016-01-12 23:36:36 +0800

    add swift basic code

---
+++ BeautyGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeautyGallery
+//
+//  Created by 周椿杰 on 16/1/12.
+//  Copyright © 2016年 周椿杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
